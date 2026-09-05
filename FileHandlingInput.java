41984ddfd7a94a43e76f37b0677d7909ea984cb4
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-09-05 19:45:51 +0500

    Add files via upload

---
+++ FileHandlingInput.java
@@ -0,0 +1,39 @@
+
+package file.handling.input;
+
+import java.io.*;
+
+public class FileHandlingInput {
+
+   
+    public static void main(String[] args) {
+        
+        
+        try{
+            
+            FileInputStream fis = new FileInputStream("C:/Users/Administrator/OneDrive/Desktop/Sample.txt");
+            
+            int i;
+            
+            while((i=fis.read())!=-1){
+                
+                System.out.println((char)i);
+                
+                fis.close();
+            }
+        }
+        
+        
+        
+        catch(Exception e){
+            
+            System.out.println(e.getMessage());
+            
+        }
+       
+    }
+    
+}
+        
+    
+
